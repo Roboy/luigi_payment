4ea968de7f2b6329c82750de9918bd4cf6c3eff3
BVURAL <bilal_v@hotmail.com>
2019-07-23 14:37:12 +0200

    merged coin and paypal, added qrcode features

--- modules/luigi/server_experiment.py
+++ modules/luigi/server_experiment.py
@@ -2,52 +2,94 @@ import rospy
 import RPi.GPIO as GPIO
 from roboy_cognition_msgs.srv import Payment
 from enum import Enum
+import qrcode
+import base64
+
+# Every signal takes:
+# 20 ms in fast mode
+# 50 ms in medium mode
+# 100 ms in slow mode
+MAX_COIN_WAIT_TIME = 30 # in seconds
+INPUT_PIN = 3 # Raspberry Pi GPIO pin to read coin counter output.
 
 class PaymentOptions(Enum):
 	COIN = 0
 	PAYPAL = 1
+
 
 class CoinCounter(object):
 	def __init__(self):
 		self.coin_sum = 0
 		self.last_call_time = 0
 	
-	def reset_coin_sum(self):
-		self.coin_sum = 0
-	
-	def coin_count_callback(self, channel, price):
-		self.coin_sum += 10
+	def coin_count_callback(self, channel):
+		self.coin_sum = self.coin_sum + 10 # Every signal is 10 cents.
 		self.last_call_time = rospy.get_time()
-		return 110
 
-def set_coin_counter(price):
-	GPIO.setmode(GPIO.BOARD)
-	INPUT_PIN = 3
-	GPIO.setup(INPUT_PIN, GPIO.IN)
+def handle_payment(req, coin_counter):
+	try:
+		if int(req.payment_option) == PaymentOptions.COIN:
+			rospy.loginfo('You have ' + str(MAX_COIN_WAIT_TIME) + ' seconds to insert coins!')
+			
+			starting_time = rospy.get_time()
 
-	coin_counter = CoinCounter()
+			# Reset coin_sum for every service call.
+			coin_counter.coin_sum = 0
+
+			# Check paid amount every second.
+			total_slept_time = 0
+			while coin_counter.coin_sum < req.price and total_slept_time < MAX_COIN_WAIT_TIME:
+				rospy.sleep(1)
+				total_slept_time = total_slept_time + 1
+			
+			# Before returning earlier than maximum wait time, wait for stable coin reader.
+			# Otherwise it can return less amount than actually paid.
+			if total_slept_time < MAX_COIN_WAIT_TIME:
+				while rospy.get_time() - coin_counter.last_call_time < 1:
+					rospy.sleep(1)
+
+					# Do not wait too much.
+					# Otherwise users can bully us.
+					# Bullies should lose their money.
+					if rospy.get_time() - starting_time > MAX_COIN_WAIT_TIME + 10:
+						break
+			
+			rospy.logdebug('Payment server returned ' + str(MAX_COIN_WAIT_TIME - total_slept_time) + ' seconds earlier.')
+			rospy.loginfo('You have paid ' + str(coin_counter.coin_sum) + ' cents.')
+			
+			return coin_counter.coin_sum, ''
+		elif int(req.payment_option) == PaymentOptions.PAYPAL:
+			img = qrcode.make('https://www.paypal.me/bilalvural35/' + str(req.price))
+			rospy.logdebug('QRCode has generated!')
+			
+			img_str = base64.b64encode(img.tobytes())
+			rospy.logdebug('QRCode has converted to base64!')
+			return 0, ''
 	
-	coin_count_callback_lambda = lambda channel: coin_counter.coin_count_callback(channel, price)
-	GPIO.add_event_detect(INPUT_PIN, GPIO.FALLING, callback=coin_count_callback_lambda, bouncetime=100)
-	
-	return coin_counter
-
-def handle_payment(req):
-	if int(req.payment_option) == PaymentOptions.COIN:
-		coin_counter = set_coin_counter(int(req.price))
-	elif int(req.payment_option) == PaymentOptions.PAYPAL:
-		print("Paying with paypal")
-		return 300, ''
-
+	except Exception as e:
+		return coin_counter.coin_sum, str(e)
 
 if __name__ == "__main__":
 	try:
-		rospy.init_node('payment_server')
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(INPUT_PIN, GPIO.IN)
 
-		rospy.Service('payment', Payment, handle_payment)
+		coin_counter = CoinCounter()
+		
+		GPIO.add_event_detect(INPUT_PIN, GPIO.FALLING, callback=coin_counter.coin_count_callback, bouncetime=100)
+
+		rospy.init_node('payment_server', anonymous=True, log_level=rospy.DEBUG)
+		rospy.logdebug('Payment server has initialized!')
+		
+		handle_payment_lambda = lambda req: handle_payment(req, coin_counter)
+
+		rospy.Service('payment', Payment, handle_payment_lambda)
+		rospy.logdebug('Payment server is set!')
 		
 		rospy.spin()
+	
 	except Exception as e:
-		print(e
+		rospy.logerr(str(e))
+	
 	finally:
 		GPIO.cleanup()
